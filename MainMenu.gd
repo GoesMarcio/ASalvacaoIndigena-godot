3ecf279e7eb0c40db4584a5b6dd2e9b60be8f6e5
Márcio Góes <marciopgoes@hotmail.com>
2021-10-05 03:39:21 -0300

    add main menu

---
+++ MainMenu.gd
@@ -0,0 +1,51 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartGame.grab_click_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_button_gui_input(event):
+	if event.is_pressed():
+		get_tree().change_scene("res://Main.tscn")
+
+func _on_button3_gui_input(event):
+	if event.is_pressed():
+		get_tree().quit()
+
+
+
+func _on_StartGame_mouse_entered():
+	$VBoxContainer/StartGame.texture = load("res://assets/Images/button_hover.png")
+
+
+func _on_StartGame_mouse_exited():
+	$VBoxContainer/StartGame.texture = load("res://assets/Images/button.png")
+
+
+func _on_Options_mouse_entered():
+	$VBoxContainer/Options.texture = load("res://assets/Images/button_hover.png")
+
+
+func _on_Options_mouse_exited():
+	$VBoxContainer/Options.texture = load("res://assets/Images/button.png")
+
+
+func _on_Quit_mouse_entered():
+	$VBoxContainer/Quit.texture = load("res://assets/Images/button_hover.png")
+
+
+func _on_Quit_mouse_exited():
+	$VBoxContainer/Quit.texture = load("res://assets/Images/button.png")
